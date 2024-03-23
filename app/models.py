db36ed71d6b4fafddd2607571465d605aa072d59
Joao Cruz <cruzjoao07@gmail.com>
2024-03-23 15:16:12 -0300

    Funcionalidades cadastrar produto e cadastrar forncedor concluidas

--- app/models.py
+++ app/models.py
@@ -1,13 +1,53 @@
+from datetime import datetime
+from pytz import timezone
 from app import db
 
 class Product(db.Model):
-    __tablename__ = 'Products'  # Alterado para corresponder ao nome da tabela no banco de dados
+    __tablename__ = 'Products'  
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(255))  # Adicionada a coluna description
-    category = db.Column(db.String(100))     # Adicionada a coluna category
+    description = db.Column(db.String(255))  
+    category = db.Column(db.String(100))     
+    last_updated = db.Column(db.DateTime, default=datetime.now(timezone('America/Sao_Paulo')), onupdate=datetime.now(timezone('America/Sao_Paulo')))  # Adicione a coluna last_updated com valor padrão
+    supplier_id = db.Column(db.Integer)  # Adicione a coluna supplier_id
+    product_code = db.Column(db.String(50))  # Adicione a coluna product_code
 
     def __repr__(self):
         return f'<Product {self.name}>'
+
+    @staticmethod
+    def generate_product_code():
+        # Chame a stored procedure GenerateProductCode aqui
+        pass
+
+    def save(self):
+        # Obtém o próximo valor de ProductCode
+        self.product_code = self.generate_product_code()
+
+        # Salva o objeto Product no banco de dados
+        db.session.add(self)
+        db.session.commit()
+
+class Supplier(db.Model):
+    __tablename__ = 'Suppliers'
+    id = db.Column('Id', db.Integer, primary_key=True)
+    name = db.Column('Name', db.String(100), nullable=False)
+    contact_name = db.Column('ContactName', db.String(255))
+    email = db.Column('Email', db.String(255))
+    phone = db.Column('Phone', db.String(20))
+
+    def __repr__(self):
+        return f'<Supplier {self.name}>'
+
+class User(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    role = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, jsonify, request
 from app import app, db
-from app.models import Product
+from app.models import Product, Supplier, User
 from sqlalchemy.exc import IntegrityError
 
 @app.route('/')
@@ -12,14 +12,14 @@ def index():
 def generate_report():
     try:
         products = Product.query.all()
-        result = [{"id": product.id, "name": product.name, "price": product.price, "quantity": product.quantity, "category": product.category} for product in products]
-        print(result)  # Adiciona esta linha para imprimir os dados antes de enviá-los para o frontend
+        result = [{"product_Code": product.product_code, "name": product.name, "price": product.price, "quantity": product.quantity, "description": product.description, "category": product.category, "last_updated": product.last_updated} for product in products]
+        print(result)  
         return jsonify(result), 200
     except Exception as e:
         return jsonify({"error": "Erro ao gerar o relatório: " + str(e)}), 500
 
 # Rota para atualizar o estoque de um produto
-@app.route('/update-stock', methods=['POST'])
+@app.route('/atualizar-estoque', methods=['POST'])
 def update_stock():
     try:
         data = request.json
@@ -38,26 +38,55 @@ def update_stock():
     except Exception as e:
         return jsonify({"error": "Erro ao atualizar o estoque: " + str(e)}), 500
 
-# Rota para cadastrar um novo produto
 @app.route('/cadastrar-produto', methods=['POST'])
 def cadastrar_produto():
     try:
+        # Chame o procedimento armazenado para obter o próximo código de produto
+        cursor = db.session.connection().connection.cursor()
+        cursor.execute("EXEC SPGenerateProductCode")
+        next_product_code = cursor.fetchone()[0]
+
         data = request.form
         name = data.get('name')
+        supplier_id = data.get('supplier_id')  
         if not name:
             return jsonify({"error": "O nome do produto é obrigatório"}), 400
 
         description = data.get('description', '')
-        price = float(data.get('price', 0))  # Convertendo para float
-        quantity = int(data.get('quantity', 0))  # Convertendo para int
+        price = float(data.get('price', 0))  
+        quantity = int(data.get('quantity', 0))  
         category = data.get('category', '')
 
-        new_product = Product(name=name, description=description, price=price, quantity=quantity, category=category)
+        new_product = Product(name=name, description=description, price=price, quantity=quantity, category=category, supplier_id=supplier_id, product_code=next_product_code)
         db.session.add(new_product)
         db.session.commit()
-        return jsonify({"message": "Produto cadastrado com sucesso!"}), 201
+        return jsonify({"message": f"Produto cadastrado com sucesso! Código do produto: {next_product_code}"}), 201
     except Exception as e:
         return jsonify({"error": "Erro ao cadastrar o produto: " + str(e)}), 500
+
+# Rota para cadastrar um novo fornecedor
+@app.route('/cadastrar-fornecedor', methods=['POST'])
+def cadastrar_fornecedor():
+    try:
+        data = request.form
+        name = data.get('supplier-name')
+        contact_name = data.get('supplier-contact')
+        email = data.get('supplier-email')
+        phone = data.get('supplier-phone')
+        
+        if not name or not contact_name:
+            return jsonify({"error": "O nome e as informações de contato do fornecedor são obrigatórios"}), 400
+
+        # Crie uma instância do modelo Supplier com os dados fornecidos
+        new_supplier = Supplier(name=name, contact_name=contact_name, email=email, phone=phone)
+        
+        # Adicione o novo fornecedor ao banco de dados
+        db.session.add(new_supplier)
+        db.session.commit()
+        
+        return jsonify({"message": "Fornecedor cadastrado com sucesso!"}), 201
+    except Exception as e:
+        return jsonify({"error": "Erro ao cadastrar o fornecedor: " + str(e)}), 500
 
 # Rota para lidar com erros 404 (recurso não encontrado)
 @app.errorhandler(404)
